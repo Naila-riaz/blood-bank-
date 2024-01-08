8290ad813e87b9d33c7eb006c453e5fa4d326948
Naila-riaz <144718946+Naila-riaz@users.noreply.github.com>
2024-01-08 15:53:18 +0500

    first commit

---
+++ login.java
@@ -0,0 +1,27 @@
+
+
+const loginForm = document.querySelector('#loginForm');
+
+
+loginForm.addEventListener('button', output) 
+  function output(){ 
+
+  
+  const email = document.querySelector('#email').value;
+  const password = document.querySelector('#password').value;
+
+  
+  
+  loginForm.reset();
+};
+
+const googleBtn = document.querySelector('#googleBtn');
+googleBtn.addEventListener('click', function() {
+  
+});
+
+
+const facebookBtn = document.querySelector('#facebookBtn');
+facebookBtn.addEventListener('click', function() {
+  
+});
